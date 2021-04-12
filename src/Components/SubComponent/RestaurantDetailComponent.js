c37588d2116b69efae24fffc865c0687b82fa7f8
Ayush <ayushtiwari200900@gmail.com>
2021-04-12 18:44:01 +0530

    designed restaurant detail

--- src/Components/SubComponent/RestaurantDetailComponent.js
+++ src/Components/SubComponent/RestaurantDetailComponent.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import MenuPopup from '../MenuPopup'
 import DishesList from './DishesList'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import Card from 'react-bootstrap/Card'
 
 const RestaurantDetailComponent = (props) => {
     const restaurantDetail = props.restaurantDetail
@@ -42,29 +43,29 @@ const RestaurantDetailComponent = (props) => {
             })
     }
 
-    const onHide=() => setModalShow(false)
-    const seeModalShow = () => {setModalShow(true)}
+    const onHide = () => setModalShow(false)
+    const seeModalShow = () => { setModalShow(true) }
 
     const addDishes = (e) => {
         console.log(menuItem)
         e.preventDefault()
         axios.post(`http:///localhost:4000/app/dishes`, menuItem)
-        .then(res => {
-            onHide()
-            showDishes()
-        })
-        .catch(e => console.log(e))
+            .then(res => {
+                onHide()
+                showDishes()
+            })
+            .catch(e => console.log(e))
     }
 
     const showDishes = () => {
         axios.get(`http://localhost:4000/app/dishes`)
-        .then(res => {
-            setDishList(res.data)
-        }).catch(e => console.log(e))
+            .then(res => {
+                setDishList(res.data)
+            }).catch(e => console.log(e))
     }
 
     const showDishesList = () => {
-        if(dishList.length != 0) {
+        if (dishList.length != 0) {
             return (
                 dishList.map((item, index) => {
                     return <DishesList key={index} item={item} />
@@ -76,10 +77,25 @@ const RestaurantDetailComponent = (props) => {
     return (
         <div>
             <div className="aboutRestaurant">
-                <h1>{restaurantDetail[0] .restaurantName}</h1>
-                <h2>{restaurantDetail[0].restaurantDiscription}</h2>
-                <p>Address: {restaurantDetail[0].restaurantAddress}</p>
-                <p>Phone: {restaurantDetail[0].restaurantPhone}</p>
+                <Card
+                    bg={'Primary'.toLowerCase()}
+                    text={'Primary'.toLowerCase() === 'light' ? 'dark' : 'white'}
+                    style={{ width: '48rem', textAlign: 'center' }}
+                    className="m-2"
+                >
+                    <Card.Header><span style={{color: 'yellow', fontSize: '1.2rem', textAlign: 'center'}}>Restaurant Detail</span></Card.Header>
+                    <Card.Body>
+                        <Card.Title><span style={{color: 'white', fontSize: '2rem', textAlign: 'center'}}>{restaurantDetail[0].restaurantName}</span></Card.Title>
+                        <Card.Text>
+                            <span style={{color: 'white', fontSize: '1.3rem', textAlign: 'center'}}>{restaurantDetail[0].restaurantDiscription}</span>
+                            <span style={{color: 'white', fontSize: '1rem', textAlign: 'center'}}><p>Address: {restaurantDetail[0].restaurantAddress}</p></span>
+                            <span style={{color: 'white', fontSize: '1rem', textAlign: 'center'}}><p>Phone: {restaurantDetail[0].restaurantPhone}</p></span>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                {/* <h2>{restaurantDetail[0].restaurantDiscription}</h2> */}
+                {/* <p>Address: {restaurantDetail[0].restaurantAddress}</p>
+                <p>Phone: {restaurantDetail[0].restaurantPhone}</p> */}
                 <Button variant="success" onClick={() => setModalShow(true)}>Add Item to Restaurant</Button>
                 <MenuPopup menuItem={menuItem} ItemHandler={ItemHandler} addDishes={addDishes} show={modalShow} onHide={onHide} />
                 {showDishesList()}
